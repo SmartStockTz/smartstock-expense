de441c553793952f7f8d2eeafcca7c22d78a991e
joshua mshana <mama27j@gmail.com>
2020-11-03 16:03:01 +0300

    update angular core

--- projects/stocks/src/pages/transfer.page.ts
+++ projects/stocks/src/pages/transfer.page.ts
@@ -5,12 +5,25 @@ import {MatTableDataSource} from "@angular/material/table";
 @Component({
   selector: 'smartstock-stocks-index',
   template: `
-    <mat-sidenav-container>
-      <mat-sidenav class="match-parent-side" #sidenav [mode]="enoughWidth()?'side': 'over'" [opened]="enoughWidth()">
+    <!--    <mat-sidenav-container>-->
+    <!--      <mat-sidenav class="match-parent-side" #sidenav [mode]="" [opened]="enoughWidth()">-->
+    <!--        -->
+    <!--      </mat-sidenav>-->
+    <!--      <mat-sidenav-content style="height: 100vh">-->
+    <!--        <smartstock-toolbar [heading]="'Transfer'" [sidenav]="sidenav"></smartstock-toolbar>-->
+
+    <!--      </mat-sidenav-content>-->
+    <!--    </mat-sidenav-container>-->
+
+    <smartstock-layout-sidenav [body]="body"
+                               [leftDrawer]="leftDrawer"
+                               [leftDrawerMode]="enoughWidth()?'side': 'over'"
+                               [leftDrawerOpened]="enoughWidth()"
+                               [heading]="'Transfer'">
+      <ng-template #leftDrawer>
         <smartstock-drawer></smartstock-drawer>
-      </mat-sidenav>
-      <mat-sidenav-content style="height: 100vh">
-        <smartstock-toolbar [heading]="'Transfer'" [sidenav]="sidenav"></smartstock-toolbar>
+      </ng-template>
+      <ng-template #body>
         <div class="container col-xl-10 col-lg-10 col-sm-9 col-md-9 col-sm-12 col-10" style="padding: 16px 0">
           <div style="margin-top: 24px">
             <smartstock-stock-transfers-table-actions></smartstock-stock-transfers-table-actions>
@@ -19,8 +32,8 @@ import {MatTableDataSource} from "@angular/material/table";
             </mat-card>
           </div>
         </div>
-      </mat-sidenav-content>
-    </mat-sidenav-container>
+      </ng-template>
+    </smartstock-layout-sidenav>
   `
 })
 
